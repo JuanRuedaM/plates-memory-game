bc265722009f3e8f9291c85cc582907bf08164a7
Juan David Rueda Martinez <112894502+JuanRuedaM@users.noreply.github.com>
2023-07-28 22:00:35 -0500

    Plates memory game

---
+++ app.js
@@ -0,0 +1,93 @@
+const letters = document.querySelector("#letters")
+const numbers = document.querySelector("#numbers")
+const btnGenerate = document.querySelector(".btnNew")
+const btnVerify = document.querySelector(".btnVerify")
+const form = document.querySelector(".form")
+const inputLetters = document.querySelector(".input-letters")
+const inputNumbers = document.querySelector(".input-numbers")
+
+let newLetter 
+let newNumber
+
+form.addEventListener('click', (e) => {
+  e.preventDefault()
+})
+
+const generateRandom = () => {
+  const pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let slicedLetter = ''
+  let slicedNumber = ''
+
+  for (let i = 0; i < 3; i++) {
+    let rndLetter = Math.floor(Math.random()*26)
+    slicedLetter += pool[rndLetter]
+
+    let rndNumber = Math.floor(Math.random()*10)
+    slicedNumber += rndNumber
+  }
+  return [slicedLetter, slicedNumber]
+}
+
+btnGenerate.addEventListener('click', () => {
+
+  [newLetter, newNumber] = generateRandom()
+
+  // reset
+  letters.style.color = 'black'
+  numbers.style.color = 'black'
+  inputLetters.value = ''
+  inputNumbers.value = ''
+
+  letters.innerHTML = newLetter
+  numbers.innerHTML = newNumber
+
+  console.log(letters.innerHTML)
+  console.log(numbers.innerHTML)
+  inputLetters.focus()
+
+  let timer = setTimeout(() => {
+    letters.innerHTML = ''
+    numbers.innerHTML = ''
+  }, 1000);
+})
+
+btnVerify.addEventListener('click', () => {
+  const inputL = inputLetters.value.toUpperCase()
+  const inputN = inputNumbers.value
+
+  letters.innerHTML = newLetter
+  numbers.innerHTML = newNumber
+
+  inputL == newLetter 
+    ? letters.style.color = 'green'
+    : letters.style.color = 'red'
+
+  inputN == newNumber 
+    ? numbers.style.color = 'green'
+    : numbers.style.color = 'red'
+
+  btnGenerate.focus()
+})
+
+inputLetters.addEventListener('input', () => {
+  let inputValueLetter = inputLetters.value.toUpperCase()
+  letters.innerHTML = inputValueLetter
+
+  if(inputValueLetter.length == 3){
+    inputNumbers.focus()
+  }
+})
+
+inputNumbers.addEventListener('input', () => {
+  let inputValueLetter = inputNumbers.value
+
+  numbers.innerHTML = inputValueLetter
+  if(inputValueLetter.length == 3){
+    btnVerify.focus()
+  }
+})
+
+
+
+
+
